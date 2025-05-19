851c125ea8217a955061897e7bc7f206af9c4aab
Ronald-silva <ronaldsilvajob@yahoo.com>
2025-05-19 00:14:34 -0300

    ajustes dimensao das imagens e sombra do header

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
                 name: "Relógio Atlantis Masculino",
                 price: "R$ 80,00",
                 description: "Relógio clássico com Design robusto e elegante com pulseira metálica, mostrador moderno (preto ou prata) e resistência à água de até 50m. Ideal para quem busca estilo e durabilidade com ótimo custo-benefício..",
-                image: "img/atlantis.jpg",
+                image: "img/atlantis.png",
                 category: "classico"
             },
             {
@@ -13,7 +13,7 @@
                 name: "Relógio Digital G-Shock – Estilo e Resistência",
                 price: "R$ 35,90",
                 description: "Modelo esportivo com visual moderno e acabamento fosco. Resistente a impactos, com pulseira em silicone e visor digital com funções de hora, data e cronômetro. Perfeito para o dia a dia ou prática esportiva..",
-                image: "img/g-chock.jpg",
+                image: "img/g-chock.png",
                 category: "esportivo"
             },
             {
@@ -21,7 +21,7 @@
                 name: "Relógio Atlantis Gold – Conforto e Sofisticação",
                 price: "R$ 50,90",
                 description: "Modelo leve e moderno com pulseira de borracha texturizada e caixa dourada. Disponível em mostradores coloridos (preto, azul e vermelho), resistente à água até 50m. Um acessório versátil que combina estilo e praticidade para o dia a dia.",
-                image: "img/atlantis-borracha.jpg",
+                image: "img/atlantis-gold.png",
                 category: "casual"
             },
             {
@@ -29,7 +29,7 @@
                 name: "Relógio Bulgari Cassino – Luxo com Personalidade Única",
                 price: "R$ 90,90",
                 description: "Inspirado no glamour dos cassinos, esse modelo traz um mostrador temático com visual de roleta e acabamento dourado imponente. A pulseira metálica complementa o design sofisticado, ideal para quem deseja se destacar com estilo e ousadia.",
-                image: "img/bulgari-cassino.jpg",
+                image: "img/bulgari-cassino.png",
                 category: "classico"
             },
             {
@@ -37,7 +37,7 @@
                 name: "Relógio Festina Dourado – Presença e Prestígio em Cada Detalhe",
                 price: "R$ 80,90",
                 description: "Com design robusto, mostrador multifuncional e acabamento totalmente dourado, o Festina transmite imponência e sofisticação. Ideal para quem busca um relógio de alto impacto visual sem abrir mão da elegância. Um acessório que transforma qualquer ocasião em um momento de destaque..",
-                image: "img/atlantis-ouro.jpg",
+                image: "img/festina-ouro.png",
                 category: "classico"
             },
             {
@@ -45,7 +45,7 @@
                 name: "Festina Gold Blue – Sofisticação com Espírito Esportivo",
                 price: "R$ 80,00",
                 description: "Um relógio que impõe respeito com seu acabamento dourado premium e mostrador azul profundo. Equipado com cronógrafo funcional e estrutura robusta, é a escolha perfeita para quem valoriza estilo, desempenho e destaque em qualquer ocasião. Elegância esportiva com presença de luxo.",
-                image: "img/festina-1.jpg",
+                image: "img/festina-blue.png",
                 category: "classico"
             },
             {
@@ -53,7 +53,7 @@
                 name: "Festina Gold White – Impacto Visual e Sofisticação",
                 price: "R$ 80,90",
                 description: "Design imponente com pulseira dourada de alta resistência e mostrador branco detalhado que inspira elegância. Ideal para quem deseja um acessório versátil que combina com eventos casuais ou formais. Com funções de cronógrafo, é mais do que um relógio – é um símbolo de presença.",
-                image: "img/festina-3.jpg",
+                image: "img/festina-white.png",
                 category: "classico"
             },
             {
@@ -61,7 +61,7 @@
                 name: "Bulgari Hélice Azul – Design que Gira Cabeças",
                 price: "R$ 90,90",
                 description: "Um espetáculo visual com mostrador azul em formato de hélice, este relógio Bulgari une ousadia e sofisticação. Sua estrutura dourada impõe respeito, enquanto o mostrador dinâmico garante exclusividade. Ideal para quem quer se destacar com estilo e modernidade.",
-                image: "img/bulgari-blue.jpg",
+                image: "img/bulgari-blue.png",
                 category: "classico"
             },
             {
@@ -69,7 +69,7 @@
                 name: "Atlantis 2 em 1 – Elegância e Tecnologia à Prova d'Água",
                 price: "R$ 90,90",
                 description: "Combinando mostrador analógico clássico e visor digital funcional, o Atlantis 2 em 1 oferece praticidade sem perder o estilo. Disponível nas versões branca e dourada, este modelo é resistente à água (50M) e ideal para quem busca versatilidade e presença marcante no pulso.",
-                image: "img/atlantis2em1.jpg",
+                image: "img/atlantis2em1.png",
                 category: "classico"
             }
         ];
